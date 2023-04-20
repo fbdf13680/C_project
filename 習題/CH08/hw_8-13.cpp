f455f827a9d7cd9ed49c62e4758500f96cbbb5ac
Jia-Wei <d46384@gmail.com>
2023-04-20 21:13:47 +0800

    uncomplete 8-13

---
+++ 習題/CH08/hw_8-13.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int pow2 (int n)
+{
+	int i, total=1;
+	for(i=1;i<=n;i++)
+		total *= 2;
+	
+	return total;
+}
+double my_fun (int n)
+{
+	if(n>1)
+		return (1.0/pow2(n))+my_fun(n-1);
+	else
+		return 0.5;
+}
+
+int main (void)
+{
+	int i;
+	for(i=3;i<=6;i++)
+		printf("my_fun(%d)=%f\n", i, my_fun(i));
+		
+	system("pause");
+	return 0;
+}
